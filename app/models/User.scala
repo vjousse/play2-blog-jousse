caa535a27f992aedcf5764d578dd817904fb596d
Vincent Jousse <vincent@jousse.org>
2011-12-02 23:30:10 +0100

    Use md5 + salt to match the password

--- app/models/User.scala
+++ app/models/User.scala
@@ -1,17 +1,48 @@
 package models
+import java.security.MessageDigest
+import java.math.BigInteger
+import play.api.Play
 
-case class User(email: String, name: String, password: String)
+case class User(username: String, password: String)
 
 object User {
 
   /**
    * Authenticate a User.
+   *
+   * The values are taken from the global conf/application.con file
    */
-  def authenticate(email: String, password: String): Option[User] = {
-    //What an authentication
-    Some(User(email, email, password))
+  def authenticate(username: String, password: String): Option[User] = {
+
+    val adminUsername = conf("blog.username") getOrElse("")
+    val adminPassword = conf("blog.password") getOrElse("")
+    val secretKey = conf("application.secret") getOrElse("")
+
+    if (
+      adminUsername == username &&
+      adminPassword == md5AsString(md5AsString(password) + md5AsString(secretKey))
+    )
+      Some(User(username, password))
+    else
+      None
   }
 
+  private def conf(key: String) = Play.unsafeApplication.configuration get key map (_.value)
+
+  private def md5AsString(s: String) = {
+
+    //We love JAVA, yes we do. Repeat after me
+    val digest = MessageDigest.getInstance("MD5").digest(s.getBytes("UTF-8"))
+
+    val bigInt = new BigInteger(1,digest)
+    var hashtext = bigInt.toString(16)
+    // Now we need to zero pad it if we actually want the full 32 chars.
+    while(hashtext.length() < 32 ){
+      hashtext = "0"+hashtext;
+    }
+
+    hashtext
+  }
 
 }
 
