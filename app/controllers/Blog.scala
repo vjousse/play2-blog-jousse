1033a3ef32cbadbf89ebdf36ba1963b72d89c912
Vincent Jousse <vincent@jousse.org>
2011-12-14 23:46:55 +0100

    Move Security actions to a dedicated controller

--- app/controllers/Blog.scala
+++ app/controllers/Blog.scala
@@ -9,43 +9,6 @@ import jousse.models._
 
 object Blog extends Controller with Secured {
 
-  // -- Authentication
-
-  val loginForm = Form(
-    of(
-      "username" -> text,
-      "password" -> text
-    ) verifying ("Invalid username or password", result => result match {
-      case (username, password) => User.authenticate(username, password).isDefined
-    })
-  )
-
-  /**
-   * Login page.
-   */
-  def login = Action { implicit request =>
-    Ok(html.login(loginForm))
-  }
-
-  /**
-   * Handle login form submission.
-   */
-  def authenticate = Action { implicit request =>
-    loginForm.bindFromRequest.fold(
-      formWithErrors => BadRequest(html.login(formWithErrors)),
-      user => Redirect(routes.Blog.admin).withSession("username" -> user._1)
-    )
-  }
-
-  /**
-   * Logout and clean the session.
-   */
-  def logout = Action {
-    Redirect(routes.Blog.login).withNewSession.flashing(
-      "success" -> "You've been logged out"
-    )
-  }
-
   def list() = {
     Action {
       implicit request =>
--- app/controllers/Security.scala
+++ app/controllers/Security.scala
@@ -1,5 +1,52 @@
 package controllers
 import play.api.mvc._
+import play.api.data._
+
+import views._
+import jousse.models._
+
+
+object Login extends Controller  {
+
+  // -- Authentication
+
+
+  val loginForm = Form(
+    of(
+      "username" -> text,
+      "password" -> text
+    ) verifying ("Invalid username or password", result => result match {
+      case (username, password) => User.authenticate(username, password).isDefined
+    })
+  )
+
+  /**
+   * Login page.
+   */
+  def form = Action { implicit request =>
+    Ok(html.login(loginForm))
+  }
+
+  /**
+   * Handle login form submission.
+   */
+  def authenticate = Action { implicit request =>
+    loginForm.bindFromRequest.fold(
+      formWithErrors => BadRequest(html.login(formWithErrors)),
+      user => Redirect(routes.Blog.admin).withSession("username" -> user._1)
+    )
+  }
+
+  /**
+   * Logout and clean the session.
+   */
+  def logout = Action {
+    Redirect(routes.Login.form).withNewSession.flashing(
+      "success" -> "You've been logged out"
+    )
+  }
+
+}
 
 /**
  * Provide security features
@@ -14,7 +61,7 @@ trait Secured {
   /**
    * Redirect to login if the user in not authorized.
    */
-  private def onUnauthorized(request: RequestHeader) = Results.Redirect(routes.Blog.login).withNewSession.flashing(
+  private def onUnauthorized(request: RequestHeader) = Results.Redirect(routes.Login.form).withNewSession.flashing(
           "error" -> "You have to be logged in to access this page"
         )
   // --
