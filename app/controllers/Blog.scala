bcaa69e598048989000430ccae81c5242c2324ca
Vincent Jousse <vincent@jousse.org>
2011-12-15 14:17:11 +0100

    Add create post form

--- app/controllers/Blog.scala
+++ app/controllers/Blog.scala
@@ -3,11 +3,20 @@ package controllers
 import play.api._
 import play.api.mvc._
 import play.api.data._
+import play.api.data.format.Formats._
+import play.api.data.validation.Constraints._
 
 import views.html._
 import jousse.models._
 
 object Blog extends Controller with Secured {
+
+  val postForm = Form(
+    of(
+      "title"   -> of[String].verifying(required),
+      "content" -> of[String].verifying(required)
+    )
+  )
 
   def list() = {
     Action {
@@ -21,7 +30,11 @@ object Blog extends Controller with Secured {
   }
 
 
+  def createPostForm() = IsAuthenticated { _ => implicit request =>
+      Ok(blog.admin.createPost(postForm))
+  }
+
   def createPost() = IsAuthenticated { _ => implicit request =>
-      Ok(blog.admin.createPost())
+      Ok(blog.admin.createPost(postForm))
   }
 }
