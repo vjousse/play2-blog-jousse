6fa344a4a1daa12a2649888dbd7b243f072e9670
Vincent Jousse <vincent@jousse.org>
2011-12-22 22:19:38 +0100

    Add package object

---
+++ app/package.scala
@@ -0,0 +1,16 @@
+package object jousse {
+
+  /**
+  * K combinator implementation
+  * Provides oneliner side effects
+  * See http://hacking-scala.posterous.com/side-effecting-without-braces
+  */
+
+  implicit def addKcombinator[A](any: A) = new {
+    def kCombinator(sideEffect: A => Unit): A = {
+      sideEffect(any)
+      any
+    }
+    def ~(sideEffect: A => Unit): A = kCombinator(sideEffect)
+  }
+}
