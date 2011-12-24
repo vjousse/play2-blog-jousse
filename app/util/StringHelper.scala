9a87308e4a2120a8e0b42f0bedb71697fdc358f1
Vincent Jousse <vincent@jousse.org>
2011-12-24 10:25:54 +0100

    Add string helper class

---
+++ app/util/StringHelper.scala
@@ -0,0 +1,33 @@
+package kcacup
+package util
+
+import com.twitter.util.Time
+import com.twitter.util.Duration
+import com.twitter.conversions.time._
+import scala.math._
+import java.text.SimpleDateFormat
+import java.util.Date
+import net.sf.jfuzzydate.{FuzzyDateFormat, FuzzyDateFormatter}
+
+object StringHelper extends StringHelper
+
+trait StringHelper {
+
+  private object Slugifier {
+    def slugify(text: String) = trimUnderscores(removeNonWord(text))
+
+    def removeNonWord(text: String) = """\W+""".r.replaceAllIn(text, "_")
+
+    def trimUnderscores(text: String) = """^\_|\_$""".r.replaceAllIn(text, "")
+  }
+
+  def slugify(text: String): String = Slugifier.slugify(text)
+
+  def formatDate(date: Time) = date format "dd MMMM yyy"
+
+  def formatDistance(date: Date) = fuzzyFormatter formatDistance date
+
+  private val fuzzyFormatter = FuzzyDateFormat.getInstance
+
+  def pluralize(s: String, n: Int) = "%d %s%s".format(n, s, if (n > 1) "s" else "")
+}
--- project/Build.scala
+++ project/Build.scala
@@ -8,12 +8,14 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0"
 
     val appDependencies = Seq(
+      "com.twitter" % "util-core" % "1.12.4",
       "com.mongodb.casbah" %% "casbah" % "2.1.5-1",
       "com.novus" %% "salat-core" % "0.0.8-SNAPSHOT"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
       resolvers ++= Seq(
+        "twitter.com" at "http://maven.twttr.com/",
         "repo.novus rels" at "http://repo.novus.com/releases/",
         "repo.novus snaps" at "http://repo.novus.com/snapshots/"
       )
