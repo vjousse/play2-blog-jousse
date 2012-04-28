dda55b0194e16300361d0d7c639559973525a1f2
Vincent Jousse <vincent@jousse.org>
2012-04-28 23:07:11 +0200

    Remove all admin/mongo stuff, let's only use markdown files

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,7 @@ object ApplicationBuild extends Build {
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
       resolvers ++= Seq(
-        "twitter.com" at "http://maven.twttr.com/",
+        "twitter.com" at "http://maven.twttr.com/"
       )
     )
 
