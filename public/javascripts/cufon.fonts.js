b043411bcbd4a91bacc4f9d2cf61225bada6d89f
Vincent Jousse <vincent@jousse.org>
2012-04-28 22:02:55 +0200

    Stop using crazy javascript fonts

--- public/javascripts/cufon.fonts.js
+++ public/javascripts/cufon.fonts.js
@@ -1,11 +1,11 @@
-Cufon.replace('#logo', { fontFamily: 'Coolvetica' });  
-Cufon.replace('#footer-logo', { fontFamily: 'Coolvetica' });  
-Cufon.replace('h1', { fontFamily: 'Coolvetica' }); 
-Cufon.replace('h2', { fontFamily: 'Coolvetica' }); 
-Cufon.replace('#slogan', { fontFamily: 'Avenir' }); 
-Cufon.replace('#title', { fontFamily: 'Avenir' }); 
-Cufon.replace('#fourofour', { fontFamily: 'Amperzand' }); 
-Cufon.replace('#fourofour-message', { fontFamily: 'Avenir' }); 
+//Cufon.replace('#logo', { fontFamily: 'Coolvetica' });  
+//Cufon.replace('#footer-logo', { fontFamily: 'Coolvetica' });  
+//Cufon.replace('h1', { fontFamily: 'Coolvetica' }); 
+//Cufon.replace('h2', { fontFamily: 'Coolvetica' }); 
+//Cufon.replace('#slogan', { fontFamily: 'Avenir' }); 
+//Cufon.replace('#title', { fontFamily: 'Avenir' }); 
+//Cufon.replace('#fourofour', { fontFamily: 'Amperzand' }); 
+//Cufon.replace('#fourofour-message', { fontFamily: 'Avenir' }); 
 
 
 // Function to include the cufon font files in to every page
@@ -14,6 +14,6 @@ function IncludeJavaScript(jsFile)
   document.write('<script type="text/javascript" src="/'
     + jsFile + '"></scr' + 'ipt>'); 
 }
-IncludeJavaScript('static/javascripts/Coolvetica.font.js');
-IncludeJavaScript('static/javascripts/Amperzand.font.js');
-IncludeJavaScript('static/javascripts/Avenir.font.js');
+//IncludeJavaScript('static/javascripts/Coolvetica.font.js');
+//IncludeJavaScript('static/javascripts/Amperzand.font.js');
+//IncludeJavaScript('static/javascripts/Avenir.font.js');
