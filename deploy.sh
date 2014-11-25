9da2b5209e2f56b4221fffb14eaa82afd28bf41b
Vincent Jousse <vincent@jousse.org>
2014-11-25 23:10:17 +0100

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+REMOTE="vjousse@marty.jousse.org"
+REMOTE_DIR="/home/vjousse/scala/blog"
+
+echo "Deploy to $REMOTE:$REMOTE_DIR"
+
+sbt ";stage;exit"
+if [ $? != 0 ]; then
+  echo "Deploy canceled"
+  exit 1
+fi
+
+RSYNC_OPTIONS=" \
+  --archive \
+  --force \
+  --delete \
+  --progress \
+  --compress \
+  --checksum \
+  --verbose \
+  --exclude conf/application.conf \
+  --exclude logs \
+  --exclude RUNNING_PID";
+
+echo "Rsync scripts, binaries and assets"
+stage="target/universal/stage"
+rsync_command="rsync $RSYNC_OPTIONS $stage/bin $stage/lib public $REMOTE:$REMOTE_DIR"
+echo "$rsync_command"
+$rsync_command
+
